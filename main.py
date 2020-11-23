ace530e5ccc0913e0beaf1a9dee987f3df56504f
YJC <424586730@qq.com>
2020-11-23 21:41:54 +0800

    update

--- main.py
+++ main.py
@@ -2,43 +2,114 @@
 import sys
 import time
 import random
-
+import eyed3
+from PyQt5 import sip
 from PyQt5.QtCore import QTimer, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFileDialog, QTableWidgetItem, QTableView
 from qtpy import QtMultimedia
+from musicWindow import Ui_MusicWindow
+from MusicPlayerMainWindow import Ui_MusicPlayerMainWindow
+import os
+class MusicWindow(QWidget, Ui_MusicWindow):
+    def __init__(self, *args, **kwargs):
+        super(QWidget, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.attrInit()
+    def attrInit(self):
+        self.fileDialog=QFileDialog(self,'选择你的音乐..','.',"music (*.mp3 *.wav *.flav)")
+        self.fileDialog.finished.connect(self.musicReloaded)
+        self.fileDialog.setModal(1)
+        self.fileDialog.setViewMode(QFileDialog.Detail)
+        self.pushButton_2.clicked.connect(self.fileDialog.show)
+        self.tableWidget.setColumnCount(6)
+        self.musicLoaded()
 
-from mainwindow import Ui_MainWindow
+        pass
+    def musicReloaded(self):
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+        musicCache = []
+        self.tableWidget.setRowCount(len(self.fileDialog.selectedFiles()))
+        with open('musicContent.cache', 'w+') as f:
+            for i, file in enumerate(self.fileDialog.selectedFiles()):
+                musicCache.append(file)
+                mp3 = eyed3.load(file)
+
+                self.tableWidget.setItem(i, 0, QTableWidgetItem(mp3.tag.title))
+                self.tableWidget.setItem(i, 1, QTableWidgetItem(mp3.tag.artist))
+                self.tableWidget.setItem(i, 2, QTableWidgetItem(mp3.tag.album))
+                self.tableWidget.setItem(i, 3, QTableWidgetItem(str(mp3.info.time_secs / 60)[:4] + 'min'))
+                self.tableWidget.setItem(i, 4, QTableWidgetItem(str(mp3.info.size_bytes / 1024 / 1024)[:5] + 'M'))
+            f.write(str(musicCache))
+    def musicLoaded(self):
+        if not os.path.exists('musicContent.cache'):
+            with open('musicContent.cache','w+') as cache:
+                cache.write('first init..')
+
+        with open('musicContent.cache') as cache:
+            musicContent=cache.read()
+            if musicContent !="[]":
+                self.tableWidget.setRowCount(len(eval(musicContent)))
+                for i, file in enumerate(eval(musicContent)):
+                    mp3 = eyed3.load(file)
+                    self.tableWidget.setItem(i, 0, QTableWidgetItem(mp3.tag.title))
+                    self.tableWidget.setItem(i,1, QTableWidgetItem(mp3.tag.artist))
+                    self.tableWidget.setItem(i, 2, QTableWidgetItem(mp3.tag.album))
+                    self.tableWidget.setItem(i, 3, QTableWidgetItem(str(mp3.info.time_secs / 60)[:4] + 'min'))
+                    self.tableWidget.setItem(i, 4,QTableWidgetItem(str(mp3.info.size_bytes / 1024 / 1024)[:5] + 'M'))
+
+
+
+
+
+
+class MainWindow(QMainWindow, Ui_MusicPlayerMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.musicWindowInit()
         self.qSliderInit()
         self.qMediaPlayerInit()
-        self.timer=QTimer()
+        self.attrInit()
+        self.toolButtonInit()
+    def musicWindowInit(self):
+        self.musicWindow=MusicWindow()
+
+        self.mainBodyLayout.addWidget(self.musicWindow)
+
+    def switchFace(self,index):
+        if index == 0:
+            self.musicMediaPlayer.pause()
+        elif index == 1:
+            self.musicMediaPlayer.play()
+        for face in list(self.face.keys()):
+            if face!=index:
+                self.face[face].setVisible(0)
+            else:
+                self.face[face].setVisible(1)
+    def toolButtonInit(self):
+        self.actionPlayList.triggered.connect(lambda :self.switchFace(0))
+        self.actionBtnMV.triggered.connect(lambda :self.switchFace(1))
+    def attrInit(self):
+        self.face={0:self.musicWindow,1:self.videoWidget}
+    def qMediaPlayerInit(self):
+        self.musicMediaPlayer=QtMultimedia.QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.musicMediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(r'mv/mv.mp4')))
+        self.videoWidget = QVideoWidget()
+        self.mainBodyLayout.addWidget(self.videoWidget)
+        self.videoWidget.setVisible(0)
+        self.musicMediaPlayer.setVideoOutput(self.videoWidget)
+    def qSliderInit(self):
+        self.styleTimer=QTimer()
         self.temp = 0
         self.tempOpac=0
         self.end=0
-        self.tempColor = 0
+        self.tempColor = random.randint(0,255)
         self.toolBarWidth=self.toolBar.size().width()
-        self.timer.timeout.connect(self.styleChange)
-        self.timer.start(11000)
-        self.timer.setInterval(30)
-    def qMediaPlayerInit(self):
-        self.myMediaPlayer=QtMultimedia.QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        self.myMediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(r'插画.mp4')))
-        self.videoWidget = QVideoWidget(self)
-        self.horizontalLayout_2.addWidget(self.videoWidget)
-        self.myMediaPlayer.setVideoOutput(self.videoWidget)
-        self.myMediaPlayer.play()
-        self.videoWidget.show()
-    def callback(self):
-        self.player.setPosition(0) # to start at the beginning of the video every time
-        self.video.show()
-        self.player.play()
-    def qSliderInit(self):
+        self.styleTimer.timeout.connect(self.styleChange)
+        self.styleTimer.start()
+        self.styleTimer.setInterval(10)
         self.horizontalSlider.setStyleSheet("""
         QSlider#horizontalSlider::handle {    
 width:0px;
@@ -62,19 +133,21 @@ height:40px;
                 self.temp+=self.toolBarWidth/100
             else:
                 self.end=1
+                self.tempColor= random.randint(0,255)
         else:
             if int(self.temp)>0:
                 self.temp -= self.toolBarWidth/100
             else:
                 self.end=0
+                self.tempColor = random.randint(0, 255)
         indexProportion=self.temp/self.toolBarWidth
 
         self.toolBar.setStyleSheet("""
-        background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 255, 255, 100), stop:{} rgba(255, 0, 0, 100),stop:1 rgba(255, 0, 0, 100));
-        """.format(indexProportion,indexProportion))
+        background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 255, 255, {}), stop:{} rgba(255, 0, 0,{}),stop:1 rgba(255, 0, 0,{}));
+        """.format(indexProportion,indexProportion,indexProportion,indexProportion))
         self.widget_3.setStyleSheet("""
-        background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 255, 255, 255), stop:{} rgba(255, 0, 0, 255),stop:1 rgba(255, 0, 0, 255));
-        """.format(indexProportion,indexProportion))
+        background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 255, 255, {}), stop:{} rgba(255, 0, 0, {}),stop:1 rgba(255, 0, 0, {}));
+        """.format(indexProportion,indexProportion,indexProportion,indexProportion))
 
 
 
---
+++ musicWindow.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'musicWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MusicWindow(object):
+    def setupUi(self, MusicWindow):
+        MusicWindow.setObjectName("MusicWindow")
+        MusicWindow.resize(851, 642)
+        self.verticalLayout = QtWidgets.QVBoxLayout(MusicWindow)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.widget = QtWidgets.QWidget(MusicWindow)
+        self.widget.setObjectName("widget")
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.label = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.horizontalLayout_3.addWidget(self.label)
+        self.label_2 = QtWidgets.QLabel(self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        font.setItalic(False)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_3.addWidget(self.label_2)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem)
+        self.horizontalLayout_4.addLayout(self.horizontalLayout_3)
+        self.verticalLayout.addWidget(self.widget)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(MusicWindow)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.pushButton_2 = QtWidgets.QPushButton(MusicWindow)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.tableWidget = QtWidgets.QTableWidget(MusicWindow)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, item)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.horizontalLayout_2.addWidget(self.tableWidget)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.retranslateUi(MusicWindow)
+        QtCore.QMetaObject.connectSlotsByName(MusicWindow)
+
+    def retranslateUi(self, MusicWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MusicWindow.setWindowTitle(_translate("MusicWindow", "Music"))
+        self.label.setText(_translate("MusicWindow", "本地音乐"))
+        self.label_2.setText(_translate("MusicWindow", "共1首"))
+        self.pushButton.setText(_translate("MusicWindow", "播放全部"))
+        self.pushButton_2.setText(_translate("MusicWindow", "添加音乐"))
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("MusicWindow", "歌名"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("MusicWindow", "歌手"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("MusicWindow", "专辑"))
+        item = self.tableWidget.horizontalHeaderItem(3)
+        item.setText(_translate("MusicWindow", "时长"))
+        item = self.tableWidget.horizontalHeaderItem(4)
+        item.setText(_translate("MusicWindow", "大小"))
+
+import res_rc
